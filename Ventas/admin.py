283e455cabcc8a00af251475ac0b4c33734f741e
Eduard Legarda <desarrollo@gmys.com.co>
2018-08-22 12:37:10 -0500

    modelos y vistas ok

--- Ventas/admin.py
+++ Ventas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from Ventas.models import *
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Producto)
+admin.site.register(Venta)
+admin.site.register(Cartera)
+admin.site.register(Abono)
--- Ventas/views.py
+++ Ventas/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Cliente
 # Create your views here.
 
 
 def post_list(request):
-        return render(request, 'Ventas/post_list.html', {})
+	clientes_list = Cliente.objects.filter(fecha_crea__lte=timezone.now()).order_by('nombres')
+        return render(request, 'Ventas/post_list.html', {'clientes_list': clientes_list})
+
+
+
 
 
 def index(request):
